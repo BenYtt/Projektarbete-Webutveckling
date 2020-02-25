ec7a584735da5799b056d085546ec291e8ff9104
benytt <benjamin.ytterstrom@gmail.com>
2020-02-25 19:59:22 +0100

    mergade antons branch

--- public/main.js
+++ public/main.js
@@ -2,25 +2,19 @@
 
 let inputText = $("#steamID")[0];
 
-// let submitButton = document.getElementById("sendBtn").addEventListener('click', () => checkInputText(inputText.value));
-let submitButton = $("#sendBtn")[0].click(checkInputText(inputText.value));
-let submitButton = $("#sendBtn")[0];
-let inputValue = inputText.value;
-const checkedInputValue = checkInputText(inputValue);
-submitButton.click(checkedInputValue);
+let submitButton = document.getElementById("sendBtn").addEventListener('click', () => checkInputText(inputText.value));
+// let submitButton = $("#sendBtn")[0].click(checkInputText(inputText.value));
+// let submitButton = $("#sendBtn")[0];
+// let inputValue = inputText.value;
+// const checkedInputValue = checkInputText(inputValue);
+// submitButton.click(checkedInputValue);
 
-showInfo(true);
+var gameNames = {};
 
-function showInfo(userFound) {
-    if (userFound) {
-        let x = document.getElementById("show-info");
-        x.style.display = "block";
-    }
-    else {
-        let x = document.getElementById("error-text");
-        x.style.display = "block";
-    }
-}
+function showInfo() {
+    let x = document.getElementById("show-info");
+      x.style.display = "block";
+  }
 
 // For testing without entering anything in the submitform. 
 
@@ -32,14 +26,11 @@ function showInfo(userFound) {
 //     checkInputText(inputText);
 // }
 
+//Sends form data when 'RETURN' is pressed
 inputText.onkeydown = function (e) {
     if (e.keyCode === 13) {
-        if (isValidIDFormat(inputText.value)){
-            passSteamID(inputText.value);
-        }
-        else{
-            getSteamID(inputText.value);
-        }
+        checkInputText(inputText.value);
+        showInfo();
     }
 }
 
@@ -50,27 +41,37 @@ function getLength(inputText) {
     }
 }
 
+// Gets the steamID if needed. Otherwise it passes the steamID (inputValue) to the functions that gets more player data
+function checkInputText(inputValue) {
+    if (getLength(inputValue) >= 2 && (!isValidID(inputValue))) {
+        getSteamID(inputValue);
+    }
+    else {
+        getPersonaName(inputValue);
+        getAvatarFull(inputValue);
+        getPlaytime(inputValue);
+
+        console.log("Sent steamID: " + inputValue + " to funtions.");
+    }
+}
+
 // Returns true if input-string is a 17 digit number.
-function isValidIDFormat(inputText) {
+function isValidID(inputText) {
     let validID = false;
     
     if (!isNaN(inputText) && getLength(inputText) === 17) {
         validID = true;
+        console.log("Input is a valid steamID.");
     }
     else {
         validID = false;
+        console.log("Input is not a valid steamID.");
     }
-    
+
     return validID;
 }
 
-function passSteamID(steamID) {
-    getPersonaName(steamID);  
-    getAvatar(steamID);
-    getPlayTime(steamID);
-}
-
-// Gets the steamID and returns it, returns false if not found.
+// Gets userdata from the server and calls the functions that gets more player data.
 function getSteamID(inputText) {
     axios.get("/getsteamid", {
         params: {
@@ -79,15 +80,12 @@ function getSteamID(inputText) {
     })
         .then(function (response) {
             let steamID = response.data.response.steamid;
+            getPersonaName(steamID);
+            getAvatarFull(steamID);
+            getPlaytime(steamID);
             
-            if(response.data.response.success === 1){
-                passSteamID(steamID);
-            }
-            else{
-                console.log("No user with that vanityID found");
-            }
+            console.log("Sent steamID: " + steamID + " to funtions.");
         });
-
 }
 
 // Gets the player name based of the steamID and passes it to setPlayerName
@@ -99,18 +97,14 @@ function getPersonaName(steamID) {
     })
         .then(function (response) {
             let personaName = response.data.response.players[0].personaname;
+            setPlayerName(personaName);
 
-            if (personaName != undefined) {
-                setPlayerName(personaName);
-            }
-            else {
-                console.log("personaName is undefined.");
-            }
+            console.log("Persona name set.");
         });
 }
 
 // Gets the player avatar based of the steamID and passes it to setPlayerAvatar.
-function getAvatar(steamID) {
+function getAvatarFull(steamID) {
     axios.get("/getpersonaname", {
         params: {
             steamID
@@ -118,20 +112,16 @@ function getAvatar(steamID) {
     })
         .then(function (response) {
             let url = response.data.response.players[0].avatarfull;
-            
-            if (url != undefined) {
-                setPlayerAvatar(url);
-            }
-            else {
-                console.log("Avatar URL is undefined.");
-            }
+            setPlayerAvatar(url);
+
+            console.log("Avatar set.");
         });
 }
 
 // Gets a players total playtime based of the steamiD and passes it to setPlaytime.
-function getPlayTime(steamID) {
+function getPlaytime(steamID) {
     let totalMinutesPlayed = 0;
-
+    
     axios.get("/getownedgames", {
         params: {
             steamID
@@ -139,15 +129,14 @@ function getPlayTime(steamID) {
     })
         .then(function (response) {
             const gameCount = response.data.response.game_count;
-            
-            if(gameCount != undefined)
+
             for (let i = 0; i < gameCount; i++) {
                 totalMinutesPlayed += response.data.response.games[i].playtime_forever;
-                setPlayTime(totalMinutesPlayed);
             }
-            else{
-                console.log("gameCount is undefined.");
-            }
+
+            setPlaytime(totalMinutesPlayed);
+            console.log("Time played is calculated: " + totalMinutesPlayed);
+
         });
 }
 
@@ -155,18 +144,17 @@ function getPlayTime(steamID) {
 
 function setPlayerName(name) {
     $(".profile-name")[0].innerHTML = name;
-    // document.getElementById('player-name').innerHTML = name;
+
 }
 
 function setPlayerAvatar(url) {
     $("#player-avatar")[0].src = url;
-    // document.getElementById('player-avatar').src = url;
+
 }
 
 function setPlaytime(totalMinutesPlayed) {
     let totalHoursPlayed = Math.round(totalMinutesPlayed / 60);
     $(".hours-played")[0].innerHTML = "Your hours: " + totalHoursPlayed + "h";
-    // document.getElementById('hours-played').innerHTML =
-    //     "Your hours: " + totalHoursPLyed + "h";
+
 }
 
